d3d1d143e2d56fa6c761b5029222c4d443f4f53b
Gopirudra-hub <89066386+Gopirudra-hub@users.noreply.github.com>
2024-01-10 21:04:46 +0530

    Update register.jsx

--- Frontend/src/register.jsx
+++ Frontend/src/register.jsx
@@ -16,7 +16,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post('http://localhost:5000/api/v1/auth/add-user', {
+      const res = await axios.post('backend-api-eta-ruddy.vercel.app/api/v1/auth/add-user', {
         name: formData.name,
         phoneNumber: formData.phoneNumber,
         password: formData.password,
